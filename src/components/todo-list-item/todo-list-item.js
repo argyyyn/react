01541e6e7132e4f022c506f4deb3552782633bbe
Argynbek Ysmail <argynbek_ysmail@epam.com>
2024-01-17 18:35:34 +0600

    react basics

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoListItem = ({label = 'argo', important = false}) => {
+  const style = {
+    color: important ? 'tomato' : 'black'
+  }
+  return <span className="todo-list-item" style={style}>{label}</span>
+}
+
+export default TodoListItem
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppHeader from "./components/app-header"
+import SearchPanel from "./components/search-panel"
+import TodoList from "./components/todo-list/todo-list"
+import TodoStatusFilter from "./components/item-status-fitler/todo-status-filter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const todoData = [
+    {label: 'drink coffee', important: false, id: 1},
+    {label: 'play auto chess', important: true, id: 2},
+  ]
+
+  return (
+    <div>
+      <div className="d-flex space-between">
+        <AppHeader/>
+      </div>
+      <div className="d-flex space-between">
+        <SearchPanel/>
+        <TodoStatusFilter/>
+      </div>
+
+      <TodoList data={todoData}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
