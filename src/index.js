1b5518714844aea9138c71b3874e373414bc4cfd
Argynbek Ysmail <argynbek_ysmail@epam.com>
2024-01-18 19:18:55 +0600

    react basics css

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AppHeader from "./components/app-header"
-import SearchPanel from "./components/search-panel"
-import TodoList from "./components/todo-list/todo-list"
-import TodoStatusFilter from "./components/item-status-fitler/todo-status-filter";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
 
   const todoData = [
-    {label: 'drink coffee', important: false, id: 1},
-    {label: 'play auto chess', important: true, id: 2},
-  ]
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
 
   return (
-    <div>
-      <div className="d-flex space-between">
-        <AppHeader/>
-      </div>
-      <div className="d-flex space-between">
-        <SearchPanel/>
-        <TodoStatusFilter/>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
       </div>
 
-      <TodoList data={todoData}/>
+      <TodoList todos={todoData} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root'));
