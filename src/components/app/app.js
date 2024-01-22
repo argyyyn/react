81ff848d354d42e3c0c6230885849055ddce13f5
Argynbek Ysmail <argynbek_ysmail@epam.com>
2024-01-22 17:27:23 +0600

    component state work with prev state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,47 @@
+import React, {Component} from "react";
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
+import ItemAddForm from "../item-add-form";
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  }
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex(el => el.id === id)
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      return {todoData: [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]}
+    })
+  }
+
+  render () {
+    const {todoData} = this.state
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={todoData}
+          onDeleted={id => this.deleteItem(id)}
+        />
+
+        <ItemAddForm />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
+  }
 };
-
-export default App
