35c9cb05e37fb254646df511ae8ee2a102d11a9b
Argynbek Ysmail <argynbek_ysmail@epam.com>
2024-01-24 17:05:26 +0600

    input form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -34,17 +34,24 @@ export default class App extends Component {
     })
   }
 
-  onToggleImportant = id => {
-    this.setState(() => {
-      this.setState(({todoData}) => {
-        const idx = todoData.findIndex(el => el.id === id)
+  toggleProperty = (arr, id, propName) => {
+    const idx = arr.findIndex(el => el.id === id)
+    const oldItem = arr[idx]
+    const newItem = {...oldItem, [propName]: !oldItem[propName]}
 
-      })
+    return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)]
+  }
+
+  onToggleImportant = id => {
+    this.setState(({todoData}) => {
+      return { todoData: this.toggleProperty(todoData, id, 'important') }
     })
   }
 
   onToggleDone = id => {
-    console.log('toggle done', id)
+    this.setState(({todoData}) => {
+      return { todoData: this.toggleProperty(todoData, id, 'done') }
+    })
   }
 
   addItem = (text) => {
@@ -56,10 +63,12 @@ export default class App extends Component {
 
   render () {
     const {todoData} = this.state
+    const doneCount = todoData.filter(el => el.done).length
+    const todoCount = todoData.length - doneCount
 
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,14 +2,30 @@ import React, {Component} from "react";
 import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: ''
+  }
 
-   render () {
-    const {onItemAdd} = this.props
+  onLabelChange = e => {
+    this.setState({ label: e.target.value })
+  }
 
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.onItemAdd(this.state.label)
+  }
+
+  render () {
     return (
-      <div className="item-add-form">
-        <button onClick={() => onItemAdd('argo')} className="btn btn-outline-secondary mt-3">Add item</button>
-      </div>
+      <form className="item-add-form d-flex mt-3"
+            onSubmit={this.onSubmit}>
+        <input type="text"
+               placeholder="What need to be done"
+               className="form-control mr-1"
+               onChange={this.onLabelChange}
+        />
+        <button className="btn btn-outline-secondary">Add item</button>
+      </form>
     )
   }
 }
