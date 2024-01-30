0d8aed79e9b9e2e8234fecef10e0e3156a5eb559
Argynbek Ysmail <argyyyn@gmail.com>
2024-01-30 17:32:29 +0600

    pattern: render

---
+++ star-db/src/components/people-page/people-page.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
+import ErrorIndicator from '../error-indicator/error-indicator';
+
+import './people-page.css';
+import SwapiService from "../../services/swapi-service";
+
+export default class PeoplePage extends Component {
+
+  swapiService = new SwapiService();
+
+  state = {
+    selectedPerson: 3,
+    hasError: false
+  };
+
+  componentDidCatch(error, info) {
+
+    this.setState({
+      hasError: true
+    });
+  }
+
+  onPersonSelected = (selectedPerson) => {
+    this.setState({ selectedPerson });
+  };
+
+  render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    return (
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList
+            renderItem={item => `${item.name} (${item.gender})`}
+            onItemSelected={this.onPersonSelected}
+            getData={this.swapiService.getAllPeople}/>
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId={this.state.selectedPerson} />
+        </div>
+      </div>
+    );
+  }
+}
