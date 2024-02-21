6136fd64b03016a54aa3e876fa1fbe6f91eb4b8c
Argynbek <argyyyn@gmail.com>
2024-02-21 08:20:55 +0600

    hooks

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,6 +1,26 @@
-import React from "react";
-import ReactDOM from 'react-dom'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
 
-const App = () => <p>hi</p>
+const App = () => {
+  return <HookSwitches />
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const HookSwitches = () => {
+  const [color, setColor] = useState('white')
+  
+  return (
+      <div style={{padding: '10px', background: color}}>
+        <button onClick={() => setColor('black') }>Dark</button>
+        <button onClick={() => setColor('white') }>Light</button>
+      </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
